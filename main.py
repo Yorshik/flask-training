a54cfe4b1646056b9f35c3bbe2bf2e590dae62af
Yorshik <kirkidzapp@gmail.com>
2024-02-17 13:19:20 +0300

    Реклама с картинкой

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import url_for
 
 app = Flask(__name__)
 
@@ -36,6 +37,39 @@ def image_mars():
             </body>
         </html>
     '''
+
+
+@app.route('/promotion_image')
+def promotion_image():
+    return f'''
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1"><title>Привет, Марс!</title>
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                </head>
+                <body>
+                    <h1 class="top">Жди нас, Марс!</h1>
+                    <img src=/static/img/MARS.png>
+                    <div class="alert alert-dark" role="alert">
+                      Человечество вырастает из детства
+                    </div>
+                    <div class="alert alert-success" role="alert">
+                      Человечеству мала одна планета.
+                    </div>
+                    <div class="alert alert-secondary" role="alert">
+                      Мы сделаем обитаемыми безжизненные пока планеты.
+                    </div>
+                    <div class="alert alert-warning" role="alert">
+                      И начнем с Марса!
+                    </div>
+                    <div class="alert alert-danger" role="alert">
+                      Присоединяйся!
+                    </div>
+                </body>
+            </html>
+        '''
 
 
 if __name__ == '__main__':
